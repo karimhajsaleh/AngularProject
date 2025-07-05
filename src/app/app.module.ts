b5db16bd5335b0384c5cd45aa97edcb7294be078
karimhajsaleh <k.hejsalahah@gmail.com>
2025-07-05 23:57:49 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { Page3Component } from './pages/page3/page3.component';
+import { Page4Component } from './pages/page4/page4.component';
+import { Page5Component } from './pages/page5/page5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component
   ],
   imports: [
     BrowserModule,
